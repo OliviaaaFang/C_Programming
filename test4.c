2624da443649e64415784e1be843fc041a31b1cb
OliviaaaFang <olivia.66.fll@gmail.com>
2021-10-30 16:43:17 +0800

    1030

--- test4.c
+++ test4.c
@@ -8,7 +8,12 @@ int main()
     int b = a%10;
     int c = a/100;
     int d = (a-c*100)/10;
-    printf("%d%d%d\n", b, d, c);
+    int e = b*100 + d*10 + c;
+
+    //这样输出会导致如果d、c是0，变成007之类的，所以最后输出还是要把它们变成三位数
+    // printf("%d%d%d\n", b, d, c);
+
+    printf("%d\n", e);
 
     return 0;
 
